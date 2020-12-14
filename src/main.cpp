fa7928029147a512b3b6309f1118d124b1033509
Jordan Cannon <LiteracyFanatic@gmail.com>
2020-12-14 05:28:38 -0600

    Ouput JSON
    Also accept the input as an argument for easier testing.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <QDebug>
 #include <QCoreApplication>
+#include <QJsonDocument>
 #include <QOnlineTranslator>
 
 int main(int argc, char *argv[])
@@ -7,12 +8,19 @@ int main(int argc, char *argv[])
     QCoreApplication app(argc, argv);
 
     QOnlineTranslator translator;
-    translator.translate("Hello world", QOnlineTranslator::Google);
+    translator.translate(argv[1], QOnlineTranslator::Google);
     QObject::connect(&translator, &QOnlineTranslator::finished, [&] {
         if (translator.error() == QOnlineTranslator::NoError)
-            qInfo() << translator.translation();
+        {
+            QJsonDocument doc = translator.toJson();
+            qInfo().noquote() << QString(doc.toJson(QJsonDocument::Indented));
+            exit(0);
+        }
         else
+        {
             qCritical() << translator.errorString();
+            exit(1);
+        }
     });
 
     return QCoreApplication::exec();
